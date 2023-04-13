9f046d7488dbdae84db6c697eb4d9a0e3796d9ea
djanloo <zzibecu@gmail.com>
2023-04-13 16:05:22 +0200

    Started tests synapses

--- numneur/neur.pyx
+++ numneur/neur.pyx
@@ -6,7 +6,7 @@ cdef mystic_function(v):
   return 0.04*v*v + 5.0*v + 140.0
 
 def izhikevich(double [:] I, double a=0.02, double b=0.2,double  c=-65.0, double d=6.0, double dt=0.001):
-  """Integration using explicit Euler"""
+  """Integration using explicit Euler. Stimulus is an external current (I)."""
   cdef int N = len(I), i
   cdef double [:] v = np.zeros(N)
   cdef double [:] u = np.zeros(N)
@@ -30,7 +30,7 @@ def izhikevich(double [:] I, double a=0.02, double b=0.2,double  c=-65.0, double
 
 
 def izhikevich_RK(double [:] I, double a=0.02, double b=0.2,double  c=-65.0, double d=6.0, double dt=0.001):
-  """Integration by a fourth order RUnge Kutta"""
+  """Integration by a fourth order RUnge Kutta. Stimulus is an external current (I)."""
   cdef int N = len(I), i
   cdef double [:] v = np.zeros(N)
   cdef double [:] u = np.zeros(N)
@@ -61,6 +61,82 @@ def izhikevich_RK(double [:] I, double a=0.02, double b=0.2,double  c=-65.0, dou
     k4 = dt*(mystic_function(v[i] +     k3) - fulltime_u)
 
     v[i+1] = v[i] + k1/3.0 + k2/6.0 + k3/6.0 + k4/3.0 +  dt*I[i]
+    u[i+1] = u[i] + a*dt*( b*v[i+1] - u[i])
+  
+  return np.array(v), np.array(u), firing_times
+
+
+def syn_izhikevich(double [:] g, double a=0.02, double b=0.2,double  c=-65.0, double d=6.0, double dt=0.001,
+                    double Esyn = -80.0):
+  """Integration given that the stimulus is a synaptic current. 
+  
+  g is the (time dependent) synaptic conductance generated from a firing presynaptic neuron, Esyn is the synapse potential.
+
+  The current is I_syn(t) = g(t)*( v(t) - Esyn )
+
+  Integration by explicit Euler.
+  """
+  cdef int N = len(g), i
+  cdef double [:] v = np.zeros(N)
+  cdef double [:] u = np.zeros(N)
+  cdef list firing_times = []
+
+  # Initial condition: Reset potential
+  v[0] = c
+
+  for i in range(N-1):
+
+    # Fire and reset
+    if v[i] >= 30.0:
+      v[i] = c
+      u[i] = u[i] + d
+      firing_times.append(i)
+  
+    v[i+1] = v[i] + dt*( mystic_function(v[i]) - u[i] + g[i]*(v[i] - Esyn))
+    u[i+1] = u[i] + a*dt*( b*v[i] - u[i])
+  
+  return np.array(v), np.array(u), firing_times
+
+
+def syn_izhikevich_RK(double [:] g, double a=0.02, double b=0.2,double  c=-65.0, double d=6.0, double dt=0.001, 
+                      Esyn=-80.0):
+  """Integration given that the stimulus is a synaptic current. 
+  
+  g is the (time dependent) synaptic conductance generated from a firing presynaptic neuron, Esyn is the synapse potential.
+
+  The current is I_syn(t) = g(t)*( v(t) - Esyn )
+
+  Integration by Runge-Kutta 4.
+  """  
+  cdef int N = len(g), i
+  cdef double [:] v = np.zeros(N)
+  cdef double [:] u = np.zeros(N)
+  cdef list firing_times = []
+
+  cdef double k1, k2, k3, k4
+
+  # Initial condition: Reset potential
+  v[0] = c
+
+  for i in range(N-1):
+
+    # Fire and reset
+    if v[i] >= 30.0:
+      v[i] = c
+      u[i] = u[i] + d
+      firing_times.append(i)
+
+    # Midtime estimates of u
+    midtime_u = u[i] + a* 0.5*dt*(b*v[i] - u[i])
+    fulltime_u = u[i] + a*dt*(b*v[i] - u[i])
+
+    # Runge-Kutta
+    k1 = dt*(mystic_function(v[i])          - u[i]        + g[i]*             (v[i]           - Esyn))
+    k2 = dt*(mystic_function(v[i] + 0.5*k1) - midtime_u   + 0.5*(g[i]+g[i+1])*(v[i] + 0.5*k1  - Esyn))
+    k3 = dt*(mystic_function(v[i] + 0.5*k2) - midtime_u   + 0.5*(g[i]+g[i+1])*(v[i] + 0.5*k2  - Esyn))
+    k4 = dt*(mystic_function(v[i] +     k3) - fulltime_u  + g[i+1]*           (v[i] + k3      - Esyn))
+
+    v[i+1] = v[i] + k1/3.0 + k2/6.0 + k3/6.0 + k4/3.0
     u[i+1] = u[i] + a*dt*( b*v[i+1] - u[i])
   
   return np.array(v), np.array(u), firing_times
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from numneur.neur import izhikevich_RK as izhiRK, izhikevich as izhi
+from numneur.neur import izhikevich_RK as izhiRK, izhikevich as izhi, syn_izhikevich_RK as sizi
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
@@ -10,7 +10,7 @@ fig = plt.figure(constrained_layout=True)
 axs = fig.subplot_mosaic([['V'],["I"], ["g"]],
                           gridspec_kw={'height_ratios':[3, 1, 1]}, sharex=True)
 N = 50_000
-dt = 3e-2
+dt = 1e-2
 T = np.arange(N)
 t = np.arange(N)*dt
 
@@ -20,35 +20,36 @@ bursting =   dict(c=-55.0, d=4)
 accomodating = dict(a=0.02, b=1, c=-65, d=2)
 
 I = np.zeros(N)
-I[N//50:N//5*3] = 10
-I[N//5: N//5 + 5000] = -10
+I[N//40:N//5*4] = 10
+# I[N//5: N//5 + 5000] = -10
 # I = 30*np.sin(t/50)
 v, recovery, firing_times = izhiRK(I, dt=dt, **chattering)
 
-axs["V"].plot(t, v, label="RK4")
-axs["I"].plot(t, I, label="injected current")
+axs["V"].plot(t, v, label="Neuron 1")
+axs["I"].plot(t, I, label="Neuron 1 (injected)")
 
 g = np.zeros(len(I))
-tau = 2
+tau = 10
+t_retard = 0.0
+g0 = 1.0
 for ft in firing_times:
-    mask = t > ft*dt
-    g[mask] += 10*(t[mask] - ft*dt)/tau*np.exp( -(t[mask]-ft*dt)/tau)
+    t_shift = ft*dt + t_retard
+    mask = (t >= t_shift)
+    g[mask] += g0*np.e*(t[mask] - t_shift)/tau*np.exp( -(t[mask] - t_shift)/tau)
+    axs["g"].axvline(ft*dt, lw=0.5, color="k", alpha=0.2)
 axs["g"].plot(t, g)
+Esyn = -80.0
+
+v, u, firing_times = sizi(g, Esyn = Esyn, **bursting)
+
+axs["V"].plot(t, v, label="Neuron 2")
+axs["I"].plot(t, g*(v-Esyn), label="Neuron 2 (synaptic)")
 
 
-v, u, firing_times = izhi(I, dt=dt, **chattering)
-g = np.zeros(len(I))
-tau = 2
-for ft in firing_times:
-    mask = t > ft*dt
-    g[mask] += 10*(t[mask] - ft*dt)/tau*np.exp( -(t[mask]-ft*dt)/tau)
-axs["g"].plot(t, g)
-
-axs["V"].plot(t, v, label="eEuler")
 axs["V"].legend()
+axs["V"].set_ylabel("V")
 
-axs["V"].set_ylabel("Membrane potential")
+axs["I"].legend()
 axs["I"].set_ylabel("I")
 axs["g"].set_ylabel("g")
-fig.suptitle("Izhikevich model (chattering pattern)")
 plt.show()
