11dbb3e57a0035c21c271460d905698723ecccf4
djanloo <zzibecu@gmail.com>
2023-04-15 21:39:31 +0200

    testing speed

--- my_profile.py
+++ my_profile.py
@@ -3,7 +3,8 @@
 from os import chdir
 from os.path import dirname, join
 from line_profiler import LineProfiler
-from numneur import neur
+from numneur import neur, networks
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,12 +17,22 @@ chdir(join(dirname(__file__), "numneur"))
 lp = LineProfiler()
 
 lp.add_function(neur.neuronet)
+lp.add_function(networks.watts_strogatz)
+lp.add_function(networks.parents_and_children)
+
+
 wrap = lp(neur.neuronet)
 
 dt = 1e-2
 I = 10*np.ones(10_000)
-M = 20
-g0 = np.random.uniform(0,0.5, size=(M,M))
+M = 10
+# g0 = np.random.uniform(0,0.5, size=(M,M))
+
+adj = networks.watts_strogatz(M, 6, .1)
+for i in range(M):
+    adj[i,i] = 0
+g0 = adj*np.random.uniform(0,.5, size=(M,M))
+
 bursting = dict(c=-55.0, d=4)
 
 
--- test_net.py
+++ test_net.py
@@ -1,22 +1,23 @@
 import numpy as np
 from numneur.neur import neuronet
-from numneur.networks import watts_strogatz as ws
+from numneur.networks import watts_strogatz as ws, parents_and_childs
 import matplotlib.pyplot as plt
 import networkx as nx
 np.random.seed(42)
 T = 10_000
 dt = 5e-2
 
-I = 5*np.ones(T)
+I = 20*np.ones(T)
 I[0: 1000] = 0.0
 
-M = 60
+M = 100
 
-adj = ws(M, 10, .5)
-print("generation done")
+adj = ws(M, 6, .1)
 
 for i in range(M):
     adj[i,i] = 0
+g0 = adj*np.random.uniform(0,.5, size=(M,M))
+print("generation done")
 
 graph = nx.Graph(adj)
 pos = nx.spectral_layout(graph)
@@ -24,13 +25,13 @@ pos_array = np.zeros((M,2))
 for n in pos.keys():
     pos_array[n] = pos[n]
 
-g0 = adj*np.random.uniform(0,0.5, size=(M,M))
 
 bursting = dict(c=-55.0, d=4)
 chattering = dict(c=-50.0, d=2)
 accomodating = dict(a=0.02, b=1, c=-65, d=2)
+tonic = dict(a=0.02, b=0.2, c=-65, d=6)
 
-v, f = neuronet(I, g0, Esyn=0.0, dt=dt ,**bursting)
+v, f = neuronet(I, g0, Esyn=0.0, dt=dt ,**tonic)
 print("simulation done")
 
 from matplotlib.animation import FuncAnimation
@@ -39,22 +40,21 @@ fig, (axt, axanim) = plt.subplots(2, 1)
 for vv in v:
     axt.plot(vv)
 
+axt.plot(*np.flip(f).T, ls="", marker="|", color="k")
+
 nx.draw_networkx_edges(graph, pos, width=0.5)
 
-scat = axanim.scatter(*pos_array.T, c=np.zeros(len(g0)), s=100, vmin=-90, vmax=-30)
+scat = axanim.scatter(*pos_array.T, c=np.zeros(len(g0)), s=50, vmin=-90, vmax=-30, cmap="plasma")
 timeline = axt.axvline(0, color="r")
 
-# axanim.set_xlim(min(pos_arr),1)
-# axanim.set_ylim(0,1)
 
 def update(i):
-    t = 2*i + 1700
+    t = 2*i + 2600
     scat.set_array(v[:, t])
     timeline.set_data([t,t], [0,1])
-    print(i)
-    # scat.set_offsets(np.random.uniform(0,1, size=(2, M)))
+    # print(i)
     return scat, timeline
 
-anim = FuncAnimation(fig, update, frames=500, interval=16)
-anim.save("anim3.mp4")
-# plt.show()
+anim = FuncAnimation(fig, update, frames=600, interval=16, blit=True)
+# anim.save("anim5.mp4")
+plt.show()
